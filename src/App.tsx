1eb1d6d6a4983809850766044656e82865c63284
Filipe Oliveira <filipeoliveira084@gmail.com>
2024-09-18 12:40:00 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import React, { LegacyRef, useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 import Papa from 'papaparse';
